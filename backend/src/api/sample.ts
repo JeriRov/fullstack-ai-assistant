4936e73d9270e8ef0d8cfdbec8f35241164231d6
Artem Mahey <artemmagej@gmail.com>
2023-08-16 16:18:27 +0300

    feat: setup openai

--- backend/src/api/sample.ts
+++ backend/src/api/sample.ts
@@ -2,10 +2,4 @@ import express from 'express';
 
 const router = express.Router();
 
-type EmojiResponse = string[];
-
-router.get<{}, EmojiResponse>('/', (req, res) => {
-  res.json(['1', '2', '3']);
-});
-
 export default router;
